c6d771f2533c1d098b4b0c7eb9f87cf666f50210
Maijs Garais <mai-soup@mailfence.com>
2023-07-10 23:22:52 +0100

    start working on nvim config for MERN

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,36 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("uh oh, packer not installed, stinky")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim', -- color scheme
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'L3MON4D3/LuaSnip'         -- snippets
+  use 'saadparwaiz1/cmp_luasnip' -- integrate lua snippets with cmp
+  use {
+    'nvim-lualine/lualine.nvim', -- statusline
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use 'onsails/lspkind-nvim'  -- pictograms
+  use 'hrsh7th/cmp-buffer'    -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp'  -- nvim-cmp source for nvim's built in LSP
+  use 'hrsh7th/nvim-cmp'      -- autocomplete
+  use 'neovim/nvim-lspconfig' -- LSP
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+
+  use 'nvim-lua/plenary.nvim' -- common utilities
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+end)
